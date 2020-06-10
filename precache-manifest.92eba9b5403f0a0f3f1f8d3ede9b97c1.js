bddfc0bc7265979739cc9fa73cd98baaf4651ac9
snh3003 <shahsama542@gmail.com>
2020-06-10 14:11:24 +0530

    Updates

---
+++ precache-manifest.92eba9b5403f0a0f3f1f8d3ede9b97c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969d7e68ca40a7f05be77e925f50c3b1",
+    "url": "/paintapp/index.html"
+  },
+  {
+    "revision": "6d59754d1c10c9c068da",
+    "url": "/paintapp/static/js/2.a0232257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paintapp/static/js/2.a0232257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288ca929c8bbf6579093",
+    "url": "/paintapp/static/js/main.24b675c7.chunk.js"
+  },
+  {
+    "revision": "f65ba2aef709fedfa405",
+    "url": "/paintapp/static/js/runtime-main.e97076ee.js"
+  }
+]);
