fc6457733f266b61884d455502afac15b003490e
snh3003 <shahsama542@gmail.com>
2020-06-10 14:05:47 +0530

    Paint app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Paint from './Paint'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  return(
+    <div>
+      <center>
+        <h1> A simple paint app using React Hooks </h1>
+      </center>
+    <Paint />
     </div>
-  );
+  )
 }
-
-export default App;
